5315355d62933bc23bb4503595dc125d7a2cda3d
Guilherme Garcia Rosa Dias <50960623+Guigarciarosa@users.noreply.github.com>
2021-12-26 21:00:56 -0300

    commit 3
    Create the html template and use jinja

--- querotaeessa/posts/views.py
+++ querotaeessa/posts/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, HttpResponse
+from django.utils import timezone
+from .models import Post
 # Create your views here.
 
 def homepage(request):
-    return render(request,"posts/homepage.html", {})
+    posts = Post.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
+    return render(request,"posts/homepage.html", {'posts':posts})
+    
